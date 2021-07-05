e62857ef41263e4354c7cbe51f602db213cada5a
miguel reyes <mreyes@consorcioqutubminar.com>
2021-07-05 13:44:39 -0500

    seccion 8 listo para back

--- 03-adminpro/src/app/services/sidebar.service.ts
+++ 03-adminpro/src/app/services/sidebar.service.ts
@@ -11,9 +11,10 @@ export class SidebarService {
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/' },
-        { titulo: 'ProgressBar', url: 'progress' },
         { titulo: 'Gráficas', url: 'grafica1' },
-        { titulo: 'Gráficas2', url: 'grafica2' },
+        { titulo: 'rxjs', url: 'rxjs' },
+        { titulo: 'Promesas', url: 'promesas' },
+        { titulo: 'ProgressBar', url: 'progress' },
       ]
     },
   ];
