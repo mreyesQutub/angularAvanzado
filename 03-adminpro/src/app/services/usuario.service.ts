e4ff9fef6f0cf09982cc1b96deff396cf2f6d604
miguel reyes <mreyes@consorcioqutubminar.com>
2021-08-22 11:28:02 -0500

    seccion 16 terminada fallo en singin google

--- 03-adminpro/src/app/services/usuario.service.ts
+++ 03-adminpro/src/app/services/usuario.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap, map, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+
 import { Observable, of } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
+import { CargarUsuario } from '../interfaces/cargar-usuarios.interface';
 
 import { Usuario } from '../models/usuario.model';
 
@@ -38,6 +40,13 @@ export class UsuarioService {
     return this.usuario.uid || '';
   }
 
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
 
   googleInit() {
 
@@ -101,13 +110,9 @@ export class UsuarioService {
     data = {
       ...data,
       role: this.usuario.role
-    };
+    }
 
-    return this.http.put(`${ base_url }/usuarios/${ this.uid }`, data, {
-      headers: {
-        'x-token': this.token
-      }
-    });
+    return this.http.put(`${ base_url }/usuarios/${ this.uid }`, data, this.headers );
 
   }
 
@@ -134,5 +139,35 @@ export class UsuarioService {
   }
 
   
+  cargarUsuarios( desde: number = 0 ) {
+
+    const url = `${ base_url }/usuarios?desde=${ desde }`;
+    return this.http.get<CargarUsuario>( url, this.headers )
+            .pipe(
+              map( resp => {
+                const usuarios = resp.usuarios.map( 
+                  user => new Usuario(user.nombre, user.email, '', user.img, user.google, user.role, user.uid )  
+                );
+                return {
+                  total: resp.total,
+                  usuarios
+                };
+              })
+            )
+  }
+
+
+  eliminarUsuario( usuario: Usuario ) {
+    
+      // /usuarios/5eff3c5054f5efec174e9c84
+      const url = `${ base_url }/usuarios/${ usuario.uid }`;
+      return this.http.delete( url, this.headers );
+  }
+
+  guardarUsuario( usuario: Usuario ) {
+
+    return this.http.put(`${ base_url }/usuarios/${ usuario.uid }`, usuario, this.headers );
+
+  }
 
 }
