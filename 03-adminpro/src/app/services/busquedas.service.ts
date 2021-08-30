b69684f04ca1a6c03125b4590fc905e7b86cecff
miguel reyes <mreyes@consorcioqutubminar.com>
2021-08-30 07:49:42 -0500

    seccion 17 ok sing google error

--- 03-adminpro/src/app/services/busquedas.service.ts
+++ 03-adminpro/src/app/services/busquedas.service.ts
@@ -4,6 +4,8 @@ import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
 
 import { Usuario } from '../models/usuario.model';
+import { Hospital } from '../models/hospital.model';
+import { Medico } from '../models/medico.model';
 
 const base_url = environment.base_url;
 
@@ -34,6 +36,22 @@ export class BusquedasService {
     );
   }
 
+  private transformarHospitales( resultados: any[] ): Hospital[] {
+    return resultados;
+  }
+
+  private transformarMedicos( resultados: any[] ): Medico[] {
+    return resultados;
+  }
+
+  busquedaGlobal( termino: string ) {
+
+    const url = `${ base_url }/todo/${ termino }`;
+    return this.http.get( url, this.headers );
+
+  }
+
+
   buscar( 
       tipo: 'usuarios'|'medicos'|'hospitales',
       termino: string
@@ -47,6 +65,12 @@ export class BusquedasService {
                 switch ( tipo ) {
                   case 'usuarios':
                     return this.transformarUsuarios( resp.resultados )
+
+                  case 'hospitales':
+                    return this.transformarHospitales( resp.resultados )
+
+                  case 'medicos':
+                     return this.transformarMedicos( resp.resultados )
                 
                   default:
                     return [];
